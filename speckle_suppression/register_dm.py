fixed a few bugs and oversights.  closer to proper rotation/flip treatment

speckle_suppression/register_dm.py
@@ -5,12 +5,14 @@ from collections import deque
 import matplotlib.pyplot as plt
 
 import dm
-
 import qt_clickpoints
+import sn_functions as sn
 
 sys.path.insert(0, '../')
 from common import support_functions as sf
 from common import fake_hardware as fhw
+
+import ipdb
 
 def compute_angle(x, y):
     """
@@ -49,9 +51,6 @@ def run(camera=None, aosystem=None, config=None, configspec=None,
     settings = sf.validate_config(config, configspec)
     #----------------------------------------------------------------------
     # Simulation parameters
-    #----------------------------------------------------------------------
-    #CAMERA, AO, CORONAGRAPH SETTINGS IN CONFIG FILE
-    #----------------------------------------------------------------------
     # Default settings 
     #----------------------------------------------------------------------
     
@@ -69,36 +68,41 @@ def run(camera=None, aosystem=None, config=None, configspec=None,
     # Load instruments
     #----------------------------------------------------------------------
     if camera == 'Sim' and aosystem == 'Sim':
+        #----------------------------------------------------------------------
+        #CAMERA, AO, CORONAGRAPH SIM SETTINGS IN CONFIG FILE
+        #----------------------------------------------------------------------
         CSM      = fhw.FakeCoronagraphOpticalSystem(**settings['SIMULATION']['OPTICAL_PARAMS'])
         AOSystem = fhw.FakeAODMSystem(OpticalModel=CSM, **settings['SIMULATION']['AO_PARAMS'])
-        Camera   = fhw.FakeDetector(opticalsystem=CSM,**settings['SIMULATION']['CAMERA_PARAMS'])
+        Camera   = fhw.FakeDetector(opticalsystem=CSM, **settings['SIMULATION']['CAMERA_PARAMS'])
 
     data_nospeck_raw = Camera.take_image()
-    data_nospeck = sf.reduce_images(data_nospeck_raw, xcen=xcen, ycen=ycen,
-                                              npix=cropsize)
-
-    #plt.imshow(data_nospeck, origin='lower' );plt.show()
+    data_nospeck = sf.equalize_image(data_nospeck_raw)
     
     speck = dm.make_speckle_kxy(calspot_kx, calspot_ky, calspot_amp, 0, N=22, flipy = False, flipx = False)
     AOSystem.set_dm_data(speck)
     data_ph1_raw    = Camera.take_image()
-    data_ph1        = sf.reduce_images(data_ph1_raw, xcen=xcen, ycen=ycen,
-                                              npix=cropsize)
-    #plt.imshow(data_ph1, origin='lower' );plt.show()
+    data_ph1        = sf.equalize_image(data_ph1_raw)
     
     speck = dm.make_speckle_kxy(calspot_kx, calspot_ky, calspot_amp, np.pi/2, N=22, flipy = False, flipx = False)
     AOSystem.set_dm_data(speck)
     data_ph2_raw    = Camera.take_image()
-    data_ph2        = sf.reduce_images(data_ph2_raw, xcen=xcen, ycen=ycen,
-                                              npix=cropsize)
-    #plt.imshow(data_ph2, origin='lower' );plt.show()
+    data_ph2        = sf.equalize_image(data_ph2_raw)
 
     cleaned = 0.5*(data_ph1+data_ph2)-data_nospeck
     #plt.imshow(cleaned, origin='lower' );plt.show()
-    points = qt_clickpoints.run_viewer(data = np.log(np.abs(cleaned+1)))
-    print(points)
+    points = qt_clickpoints.run_viewer(data = np.log10(np.abs(cleaned+1)))
+    assert len(points) == 2, "You must click two points"
+    c1 = np.array(sn.get_spot_centroid(cleaned, guess_spot=points[0]))
+    c2 = np.array(sn.get_spot_centroid(cleaned, guess_spot=points[1]))
+    print("Centers: ", c1, c2)
+    centered_points = qt_clickpoints.run_viewer(data = np.log10(np.abs(cleaned+1)), user_points = [c1, c2])
+    angle = compute_angle(*(c2-c1))
+    center = 0.5*(c1 + c2)
+    print("measured angle: ", angle)
+    print("measured center:", center)
+    #the /2 is because it is really -kx to kx, and -ky to ky
+    print("Measured lambda/D: ", np.linalg.norm(c2-c1)/np.linalg.norm(np.array([calspot_kx, calspot_ky]))/2, "pixels")
 
-    
 if __name__ == "__main__":
     camera = "Sim"
     aosystem = "Sim"

speckle_suppression/temp_hw_test.py
@@ -45,37 +45,23 @@ def run(camera=None, aosystem=None, config=None, configspec=None,
     
     else:
         raise ValueError("Sim only now")
-    SAN = SpeckleAreaNulling(Camera, AOSystem, 
-                               initial_probe_amplitude=1,
-                               initial_regularization=1,
-                               controlregion_iwa = 3,
-                               controlregion_owa = 8,
-                               xcenter=xcen,
-                               ycenter=ycen,
-                               Npix_foc=cropsize,
-                               lambdaoverD=4)
 
-    imax=[] 
-    ks = []
-    plt.ion()
-    for k in np.arange(3,11, 0.25):
-        speck = dm.make_speckle_kxy(k, 0, 20e-9, 0, N=22, flipy = False, flipx = False)
-        AOSystem.set_dm_data(speck)
-        data_speck = Camera.take_image()
-        data_proc = sf.equalize_image(data_speck)
-        plt.imshow(data_proc, origin='lower')
-        plt.draw()
-        plt.pause(0.1)
-        plt.clf()
-        ks.append(k)
-        imax.append(np.max(data_proc[130,:]))
-        #plt.plot(data_proc[130, :], label = str(k))
-        #plt.clf()
-        #plt.close()
-    plt.close()
+    return CSM, AOSystem, Camera
+
 if __name__ == "__main__":
-    #plotter = pf.LivePlotter()
-    
     camera = "Sim"
     aosystem = "Sim"
-    run(camera, aosystem, config='sn_config.ini', configspec='sn_config.spec')#, plotter=plotter)
+    CSM, AOSystem, Camera = run(camera, aosystem, config='sn_config.ini', configspec='sn_config.spec')
+    print("Validating no mystery rotations in the Camera")
+    fig, axes = plt.subplots(1, 2, figsize=(12, 5))
+    axes[0].imshow(Camera.take_image(), origin='lower')
+    axes[0].set_title('Camera Image')
+    axes[1].imshow(np.array(CSM.focal_efield.power.shaped), origin='lower')
+    axes[1].set_title('Focal Plane Electric Field Power')
+    plt.tight_layout()
+    plt.show()
+
+    print("Showing Poisson noise, note array indexing in the x and y are flipped")
+    for i in range(10):
+        a = Camera.take_image()
+        print(a[423, 283])